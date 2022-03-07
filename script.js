fb0ad02df4bd7aecce2367808cf3fc2a8f0ed657
Irelia <irelia@Irelias-MacBook-Pro.local>
2022-03-07 11:19:30 -0500

    colors

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function checkGuess () {
         let letterPosition = rightGuess.indexOf(currentGuess[i])
         // is letter in the correct guess
         if (letterPosition === -1) {
-            letterColor = "rgb(150,150,150)";
+            letterColor = "rgb(175,175,175)";
         } else {
             // now, letter is definitely in word
             // if letter index and right guess index are the same
