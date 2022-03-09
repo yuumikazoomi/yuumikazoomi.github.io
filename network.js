28a357084ec1a9d2a8a49e36a7fbb8fc80d56a1b
Irelia <irelia@Irelias-MacBook-Pro.local>
2022-03-09 14:26:59 -0500

    checking for uid and sending or null if empty'

--- network.js
+++ network.js
@@ -6,6 +6,19 @@ class Network{
             var received_msg = evt.data;
             callback(received_msg);
           };
+          this.ws.onopen = function(evt){
+            if(localStorage.hasOwnProperty('uid')){
+                //key exists, check if it's null
+                let selfuid = localStorage.getItem('uid');
+                if(selfuid=="null" || selfuid == "undefined"){
+                    this.send({pid:1,myuuid:"null"});
+                }else{
+                    this.send({pid:1,myuuid:selfuid});
+                }
+            }else{
+                this.send({pid:1,myuuid:"null"});
+            }
+          }
     }
     send(packet){
         this.ws,this.send(JSON.stringify(packet));
